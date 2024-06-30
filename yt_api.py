3449d0b829d78b096bc9567711efcb341d079e8f
Vivian Song <vivian.song@emory.edu>
2024-06-30 20:00:10 +0000

    Initial Commit: POST request test

---
+++ yt_api.py
@@ -0,0 +1,16 @@
+import requests
+#from pyyoutube import Api
+import os
+
+key = os.environ.get('YT_API_ID')
+base_url = "https://www.googleapis.com/youtube/v3"
+search_url = base_url + "/search"
+headers = {"part":"snippet",
+  "key":key, 
+  "type":"video", 
+  "q":"software"
+}
+#api = Api(api_key=key)
+response = requests.get(search_url, headers)
+print(response.json())
+print("hello")
