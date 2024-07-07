6209537bbdbfe7be9dd7a25788bd34e3eddb79ee
Vivian Song <vivian.song@emory.edu>
2024-07-07 18:17:10 +0000

    edited style

--- yt_sqltable.py
+++ yt_sqltable.py
@@ -1,5 +1,5 @@
 # This simple script retrieves and prints a neat
-# table of search results based on the entered 
+# table of search results based on the entered
 # search query.
 
 import requests
@@ -7,25 +7,25 @@ import os
 import sqlalchemy as db
 import pandas as pd
 
-## provide YouTube Data API key
+# provide YouTube Data API key
 key = os.environ.get('YT_API_ID')
 
-## send GET request for YouTube videos
+# send GET request for YouTube videos
 base_url = "https://www.googleapis.com/youtube/v3"
 search_url = base_url + "/search"
 query = input("Input Search Query: ")
 
-headers = {"part":"snippet",
-  "key":key, 
-  "type":"video", 
-  "q":query,
-  "maxResults":"5"
-}
+headers = {"part": "snippet",
+            "key": key, 
+            "type": "video", 
+            "q": query,
+            "maxResults": "5"
+          }
 
 response = requests.get(search_url, headers)
 query_dict = pd.DataFrame(pd.json_normalize(response.json()["items"]))
 
-## create SQL database, print as Pandas DataFrame
+# create SQL database, print as Pandas DataFrame
 engine = db.create_engine('sqlite:///query_result.db')
 query_dict.to_sql('results', con=engine, if_exists='replace', index=False)
 
